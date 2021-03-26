5679b133e9bf483742d027f4d50f4430cf40b32a
Kássio <55934898+sankassio99@users.noreply.github.com>
2021-03-26 14:34:47 -0300

    Refactoring details

--- ordenaAlfabeto.py
+++ ordenaAlfabeto.py
@@ -1,13 +1,10 @@
 def bubbleSort(arrayWord):
-
-
     for j in range(len(arrayWord) - 1):
         for s in range(len(arrayWord) - 1):
             if (arrayWord[s] > arrayWord[s+1]):
                 arrayWord[s], arrayWord[s+1] = arrayWord[s+1], arrayWord[s]
 
     return arrayWord
-
 
 print("Ordena letras em ordem alfabeta")
 word = input("Digite uma palavra")
@@ -17,3 +14,4 @@ for w in word:
     arrayWord.append(w)
 
 print(bubbleSort(arrayWord))
+
