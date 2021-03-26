7833a7c6fd69ef94c15e4fe673fe6a8f05d062cd
Kássio <55934898+sankassio99@users.noreply.github.com>
2021-03-26 12:49:04 -0300

    Using the logic

--- ordenaAlfabeto.py
+++ ordenaAlfabeto.py
@@ -1,6 +1,36 @@
 
+def bubbleSort(arrayWord):
+
+    newWordArray = []
+
+    for i in range(len(arrayWord)):
+        beforeLetter = arrayWord[i]
+        print("before " + beforeLetter)
+        for s in range(len(arrayWord) - 1):
+            afterLetter = arrayWord[s + 1]
+            print("After " + afterLetter)
+            if (beforeLetter < afterLetter):
+                newWordArray.append(beforeLetter)
+                newWordArray.append(afterLetter)
+                break
+            else:
+                newWordArray.append(arrayWord[s + 1])
+                newWordArray.append(arrayWord[i])
+                break
+
+    return newWordArray
+
+
 print("Ordena letras em ordem alfabeta")
 word = input("Digite uma palavra")
 
-palavraOrdenada = sorted(word)
-print(palavraOrdenada)
+arrayWord = []
+for w in word:
+    arrayWord.append(w)
+
+print(bubbleSort(arrayWord))
+
+
+
+
+
